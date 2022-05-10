2a0146a6c799b6ff6a2c8c0d8518cf8018ebf38e
Loïc Tosser <loic@kalvad.com>
2022-05-10 17:59:01 +0400

    Config

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 [ -z "$APP_HOME" ] && export APP_HOME=$(pwd)
-[ -z "$ATUIN_CONFIG_DIR" ] && export ATUIN_CONFIG_DIR="$APP_HOME/atuin"
+[ -z "$ATUIN_CONFIG_DIR" ] && export ATUIN_CONFIG_DIR="$APP_HOME/config"
 
 mkdir -p $ATUIN_CONFIG_DIR
 cat <<EOF > $ATUIN_CONFIG_DIR/server.toml
