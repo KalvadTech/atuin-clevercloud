b2e0dfbf63dc48fda0c8dc7d5355ba5f88835893
Loïc Tosser <loic@kalvad.com>
2022-05-10 17:54:12 +0400

    Fix more stuff

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+set -x
+[ -z "$APP_HOME" ] && export APP_HOME=$(pwd)
+[ -z "$ATUIN_VERSION" ] && export ATUIN_VERSION="0.9.1"
+
+rm -rf $APP_HOME/atuin*
+wget -O atuin.tar.gz https://github.com/ellie/atuin/releases/download/v0.9.1/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+tar xvzf atuin.tar.gz
+cp atuin-v0.9.1-x86_64-unknown-linux-gnu/atuin ./
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+set -x
+[ -z "$APP_HOME" ] && export APP_HOME=$(pwd)
+[ -z "$ATUIN_CONFIG_DIR" ] && export ATUIN_CONFIG_DIR="$APP_HOME/atuin"
+
+mkdir -p $ATUIN_CONFIG_DIR
+cat <<EOF > $ATUIN_CONFIG_DIR/server.toml
+host = "0.0.0.0"
+port = 8080
+open_registration = true
+db_uri="$POSTGRESQL_ADDON_URI"
+EOF
+
+./atuin server start
