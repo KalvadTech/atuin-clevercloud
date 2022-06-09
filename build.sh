d14440d0c2111e44f8aa76ff20e2493a3b0dd667
Loïc Tosser <loic@kalvad.com>
2022-06-09 17:23:37 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 [ -z "$APP_HOME" ] && export APP_HOME=$(pwd)
-[ -z "$ATUIN_VERSION" ] && export ATUIN_VERSION="0.9.1"
+[ -z "$ATUIN_VERSION" ] && export ATUIN_VERSION="0.10.0"
 
 rm -rf $APP_HOME/atuin*
 wget -O atuin.tar.gz https://github.com/ellie/atuin/releases/download/v0.9.1/atuin-v${ATUIN_VERSION}-x86_64-unknown-linux-gnu.tar.gz
